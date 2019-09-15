4ce5bad2f70a0b404f3e7f622f439413b4bb2b98
Gina Kendell <ginakendell@Ginas-MacBook-Pro.local>
2019-09-15 17:06:14 -0400

    first commit

---
+++ CurrenciesLab/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace CurrenciesLab
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            //Declare Variables
+            double dollar1;
+            double dollar2;
+            double dollar3;
+
+            //this is where will store calculations
+            double total;
+            double average;
+            double min;
+            double max;
+
+            //Get User Input
+            Console.Write("Please Enter a Dollar Amount: $");
+            dollar1 = double.Parse(Console.ReadLine());
+
+            //Get User Input
+            Console.Write("Please Enter a Dollar Amount: $");
+            dollar2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter a Dollar Amount: $");
+            dollar3 = double.Parse(Console.ReadLine());
+
+            //Get Total
+            total = (dollar1 + dollar2 + dollar3);
+
+            //Get Average
+            average = (dollar1 + dollar2 + dollar3 / 3);
+
+
+            // Get Max
+            max = Math.Max(dollar1, Math.Max(dollar2, dollar3));
+
+
+            // Get Min
+            min = Math.Min(dollar1, Math.Min(dollar2, dollar3));
+
+        
+      
+
+            //Print Total in US, Swedish, Japanese and Thai
+            Console.WriteLine("The Total in US is: " + total.ToString("C", CultureInfo.CreateSpecificCulture("en-us")));
+            Console.WriteLine("The Total in JP is: " + total.ToString("C", CultureInfo.CreateSpecificCulture("ja-jp")));
+            Console.WriteLine("The Total in SV is: " + total.ToString("C", CultureInfo.CreateSpecificCulture("sv-se")));
+            Console.WriteLine("The Total in TH is: " + total.ToString("C", CultureInfo.CreateSpecificCulture("th-th")));
+
+
+            Thread.Sleep(5000);
+
+        }
+
+    }
+    }
